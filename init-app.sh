48f9054b7cf616890628358a0ddb9abb83a46c7f
Param Shah <shah.paramk@northeastern.edu>
2025-02-23 17:52:33 -0500

    Updated setup.sh

--- init-app.sh
+++ init-app.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# Start the application service
+
 #sudo systemctl daemon-reload
 #sudo systemctl enable csye6225-aws
-
-if [ ! -f "/etc/systemd/system/csye6225-aws.service" ]; then
-echo "csye6225-aws.service not found. Copying..."
-sudo cp /tmp/csye6225-aws.service /etc/systemd/system/csye6225-aws.service
-sudo chmod 644 /etc/systemd/system/csye6225-aws.service
-sudo systemctl daemon-reload
-fi
--- setup.sh
+++ setup.sh
@@ -47,15 +47,20 @@ sudo useradd csye6225 --shell /usr/sbin/nologin -g csye6225
 sudo cp /tmp/csye6225-aws.service /etc/systemd/system/
 sudo cp /tmp/webapp.zip /opt/
 sudo unzip /opt/webapp.zip -d /opt/webapp
-sudo /tmp/.env /opt/webapp
+sudo cp /tmp/.env /opt/webapp
+
+cd /opt/webapp || exit
 
 
-#sudo chown -R csye6225:csye6225 /opt/webapp
-sudo chown csye6225:csye6225 /opt/webapp/.env
+sudo chown -R csye6225:csye6225 /opt/webapp
+sudo chown csye6225:csye6225 .env
 sudo npm install
-
 
 sudo mkdir -p /opt/webapp/logs
 sudo chown -R csye6225:csye6225 /opt/webapp/logs
+
+sudo systemctl daemon-reload
+sudo systemctl enable csye6225-aws
+
 
 echo "Setup completed"
